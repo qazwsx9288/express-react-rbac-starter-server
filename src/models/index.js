71e631f3912e803d144256b21af16a37df39a26a
wang2hao <378619897@qq.com>
2024-03-22 09:17:49 +0800

    fix: 修复RolePermission未能正确引入的问题

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,10 @@
 const Permission = require('./permission');
 const Role = require('./role');
 const User = require('./user');
+const RolePermission = require('./role-permission');
 
 module.exports = {
+  RolePermission,
   Permission,
   Role,
   User,

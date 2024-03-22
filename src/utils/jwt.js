d530784d7b7ec9c95bf9a79d58101630f9549006
wang2hao <378619897@qq.com>
2024-03-22 09:07:49 +0800

    fix: 修复jwt中间件生成token的bug

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,8 +2,10 @@ const jwt = require('jsonwebtoken');
 const config = require('@/config');
 
 // 生成JWT
-function generateToken(data) {
-  return jwt.sign(data, config.jwtSecret, { expiresIn: config.jwtExpire }); // '1h'可以根据需要调整
+function generateToken(user) {
+  const token = jwt.sign({ id: user.id }, config.jwtSecret, { expiresIn: config.jwtExpire }); // '1h'可以根据需要调整
+  console.log(token);
+  return token;
 }
 
 // 验证JWT
